50007a2415ae706279832bb06bff847d7cc48645
Kapil <kydv901@gmail.com>
2026-07-10 21:54:33 +0530

    Refine resilience module exports

--- aether/resilience/__init__.py
+++ aether/resilience/__init__.py
@@ -1,3 +1,20 @@
-from aether.resilience.circuit_breaker import CircuitBreaker, CircuitBreakerMetrics, CircuitBreakerState
+from aether.resilience.circuit_breaker import CircuitBreaker, State
+from aether.resilience.retry_handler import retry, RetryError
+from aether.resilience.timeout_handler import with_timeout, timeout_decorator
+from aether.resilience.bulkhead import Bulkhead, BulkheadMetrics
+from aether.resilience.degradation import DegradationManager
 
-__all__ = ["CircuitBreaker", "CircuitBreakerMetrics", "CircuitBreakerState"]
+CircuitBreakerState = State
+
+__all__ = [
+    "CircuitBreaker",
+    "CircuitBreakerState",
+    "State",
+    "retry",
+    "RetryError",
+    "with_timeout",
+    "timeout_decorator",
+    "Bulkhead",
+    "BulkheadMetrics",
+    "DegradationManager",
+]
